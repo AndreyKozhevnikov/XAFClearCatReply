efde5f7abf14aaab504d5b5921835400e70d356d
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-08-09 18:40:39 +0300

    subscribe to 'mark as reviewed' button

--- XAFClearCatReply.js
+++ XAFClearCatReply.js
@@ -1,17 +1,36 @@
-/*eslint strict: ["error", "function"]*/
+/*eslint strict: ["error", "global"]*/
+/* global ko*/
+'use strict';
 // ==UserScript==
-// @name         XAF Bug Subject Validator
+// @name         XAF Clear Cat Reply
 // @namespace    http://tampermonkey.net/
-// @version      0.1.5
-// @description  Validate the ticket Subject according to XAF team rules
-// @author       KL
+// @version      0.1.1
+// @description  clear CAT reply when a ticked is assigned to 2nd line
+// @author       AndreyK
 // @match        https://isc.devexpress.com/Thread/WorkplaceDetails*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-  'use strict';
+function subscribeToButtons() {
+  let _originalMarkAsReviewed = window.fullViewModel.bottomPanelItems.markAsReviewed;
+  window.fullViewModel.bottomPanelItems.markAsReviewed = customMarkAsRevied(_originalMarkAsReviewed);
+  ko.applyBindings(window.fullViewModel, document.getElementById('mark-as-reviewed')); // wihtout this code the 'mark-as-reviewed' button stil call the original markAsReviewed method. some kind of magic I suppose (don't want to research)
+}
+function customMarkAsRevied(originalFunction) {
+  return function() {
+    clearCatReply();
+    console.log('original');
+    originalFunction();
+  };
+}
 
+function clearCatReply() {
+  console.log('clear cat');
+  // window.fullViewModel.CatViewModel.selectedReply().value.currentValue('');
+}
 
-  window.fullViewModel.CatViewModel.selectedReply().value.currentValue('');
-})();
+subscribeToButtons();
+
+// let ctx= ko.contextFor(document.getElementById('mark-as-reviewed'))
+// ctx.$data.bottomPanelItems.markAsReviewed=function(){console.log('newtest55')}
+// ko.applyBindings(ctx.$data,document.getElementById('mark-as-reviewed'))
