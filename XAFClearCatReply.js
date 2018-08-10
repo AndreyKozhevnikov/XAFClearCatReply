41f16919f5b85444a3d48dd8aa26cfd399250dbb
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-08-10 19:06:42 +0300

    temp

--- XAFClearCatReply.js
+++ XAFClearCatReply.js
@@ -12,21 +12,38 @@
 // ==/UserScript==
 
 function subscribeToButtons() {
+  window.fullViewModel.bottomPanelItems_submitButtonClick = window.fullViewModel.bottomPanelItems.submitButtonClick;
+   // debugger;
+  window.fullViewModel.bottomPanelItems.submitButtonClick = customSubmitButtonClick();
+
   let _originalMarkAsReviewed = window.fullViewModel.bottomPanelItems.markAsReviewed;
-  window.fullViewModel.bottomPanelItems.markAsReviewed = performCustomOperationBefore(_originalMarkAsReviewed);
-  ko.applyBindings(window.fullViewModel, document.getElementById('mark-as-reviewed')); // wihtout this code the 'mark-as-reviewed' button stil call the original markAsReviewed method. some kind of magic I suppose (don't want to research)
+  window.fullViewModel.bottomPanelItems.markAsReviewed = customMarkAsReviewed(_originalMarkAsReviewed);
 
-  let _submitButtonClick = window.fullViewModel.bottomPanelItems.submitButtonClick;
-  window.fullViewModel.bottomPanelItems.submitButtonClick = performCustomOperationBefore(_submitButtonClick, isSecondLineAssigned);
-
+  let markAsReviewedButton = document.getElementById('mark-as-reviewed');
+  ko.cleanNode(markAsReviewedButton);
+  ko.applyBindings(window.fullViewModel, markAsReviewedButton); // without this code the 'mark-as-reviewed' button stil call the original markAsReviewed method. some kind of magic I suppose (don't want to research)
 }
-function performCustomOperationBefore(originalFunction, conditionFunction) {
+
+function customMarkAsReviewed(originalMarkAsReviewed) {
   return function() {
-    console.log(conditionFunction);
-    if (conditionFunction == undefined || conditionFunction())
+      console.log('0');
+    originalMarkAsReviewed();
+     // clearCatReply();
+    // window.fullViewModel.bottomPanelItems.submitButtonClick();
+      window.fullViewModel.bottomPanelItems.submitButtonClick();
+    //setTimeout(window.fullViewModel.bottomPanelItems.submitButtonClick, 500);
+  };
+}
+
+function customSubmitButtonClick() {
+  return function() {
+      console.log('1');
+    if (isSecondLineAssigned()) {
       clearCatReply();
-    console.log('original');
-    originalFunction();
+    }
+      console.log('2');
+      window.fullViewModel.bottomPanelItems_submitButtonClick();
+    //setTimeout(window.fullViewModel.bottomPanelItems_submitButtonClick,5000);
   };
 }
 
@@ -34,14 +51,11 @@ function isSecondLineAssigned() {
   return window.supportCenter.viewModel.issueDetails.selectedAssignTo.value.currentValue() == '2f795f88-e3ef-4913-9fdb-66c1e7525766'; // .!Support Reviewed Queue 2f795f88-e3ef-4913-9fdb-66c1e7525766
 }
 
-
 function clearCatReply() {
   console.log('clear cat');
-  // window.fullViewModel.CatViewModel.selectedReply().value.currentValue('');
+  window.fullViewModel.CatViewModel.selectedReply().value.currentValue('');
 }
-
+window.onload=function(){
+    console.log('-1');
 subscribeToButtons();
-
-// let ctx= ko.contextFor(document.getElementById('mark-as-reviewed'))
-// ctx.$data.bottomPanelItems.markAsReviewed=function(){console.log('newtest55')}
-// ko.applyBindings(ctx.$data,document.getElementById('mark-as-reviewed'))
+}
